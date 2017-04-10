17e7f4bcde0d5085ecce32f48b377e9ec5ce7815
Maksym Stefanko <casstefaks@gmail.com>
2017-04-10 14:53:32 +0300

    config

--- server.js
+++ server.js
@@ -20,11 +20,10 @@ app.use(bodyParser.json());
 //         credentials: corsConfig.credentials
 //   }
 // ));
-//app.use(cors());
+app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Credentials", true);
   next();
 });
 
