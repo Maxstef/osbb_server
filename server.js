311b545a421f2b0b4b35fd32667888136e07c4ae
Maksym Stefanko <casstefaks@gmail.com>
2017-04-10 14:56:49 +0300

    socket origin

--- server.js
+++ server.js
@@ -20,10 +20,11 @@ app.use(bodyParser.json());
 //         credentials: corsConfig.credentials
 //   }
 // ));
-app.use(cors());
+//app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "authorization, Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
   next();
 });
 
